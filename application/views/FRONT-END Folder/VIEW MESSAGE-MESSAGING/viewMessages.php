8e446ab53b1c95960dd7ebecd9154006fc77b794
Yee <yeepatrick01@gmail.com>
2022-01-27 13:19:35 +0800

    UI-view message

--- application/views/FRONT-END Folder/VIEW MESSAGE-MESSAGING/viewMessages.php
+++ application/views/FRONT-END Folder/VIEW MESSAGE-MESSAGING/viewMessages.php
@@ -9,7 +9,7 @@
 
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/message/style-view.css">
+	<link rel="stylesheet" href="style-view.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Goblin+One&family=Noto+Serif+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Quicksand:wght@300;400;500;600;700&family=Ranchers&family=Slackey&family=Syne:wght@400;500;600;700;800&display=swap" rel="stylesheet">
@@ -17,47 +17,35 @@
 </head>
 
 <body>
-<<<<<<< Updated upstream
-    <header>
-    <?php $this->load->view('elements/navbar');?>
- 
-<form method="POST" action="/messages/addMessage">
-=======
     <div class="container-buttons">
         <div class="for-buttons">
             <button type="button" class="buttontop">
-                <a class="button-text" href="<?php echo base_url();?>messages/inbox">
                 <span class="button-icon">
                     <ion-icon name="arrow-undo-outline"></ion-icon>
                 </span>
-                Previous Page </span></a>
+                <span class="button-text" href="<?php echo base_url();?>users/inbox">Previous Page</span>
             </button>
-            
+
             <button type="button" class="buttontop">
-                <a class="button-text" href="<?php echo base_url()?>">
                 <span class="button-icon">
                     <ion-icon name="home-outline"></ion-icon>
                 </span>
-                Back to Home </span>
-            </a></button>
-            
+                <span class="button-text" href="<?php echo base_url()?>">Back to Home</span>
+            </button>
         </div> 
     </div>
-    <?php
-        foreach($message as $msg) ?>
-    <div class="card bg-dark border border 3 p-1 m-5" style="width:70%; height:50%;">
-        <div class="message-body ">
-            <div class="heading ">
-                <h5 style="color: white"><span class="icon fas fa-envelope"></span><?php echo $_SESSION['user_uid'] ?></h5>
-               
-                <h5 style="color: white"><span class="icon fas fa-calendar"></span><?php echo(date("F j, Y", $msg['dateCreated']));?></h5>
+
+    <div class="container-message">
+        <div class="message-body">
+            <div class="heading">
+                <h5><span class="icon fas fa-envelope"></span><?php echo $_SESSION['user_id'] ?></h5>
+                <h5><span class="icon fas fa-calendar"></span><?php echo $msg['dateCreated'];?></h5>
             </div>
             <div class="message">
-                <p style="color: white"><?php echo $msg['messageContent'];?></p>
+                <p><?php echo $msg['messageContent'];?></p>
             </div>
         </div>
     </div>
->>>>>>> Stashed changes
     
     <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
     <!-- JavaScript Bundle with Popper -->
